7aa5a05a779bfc10c3009a0a3201dbbb0f685cd3
serenahua2009 <serenahua2009@gmail.com>
2023-10-23 22:39:18 +0700

    Fix provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  region = var.aws_region
+  # access_key = var.AWS_ACCESS_KEY
+  # secret_key = var.AWS_SECRET_KEY
 }
