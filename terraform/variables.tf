210d128b97734e1a4a2472eb07f633ae419dc009
serenahua2009 <serenahua2009@gmail.com>
2023-10-23 22:18:51 +0700

    Update key

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "AWS_ACCESS_KEY" {
   type        = string
   description = "AWS ACCESS KEY"
-  default     = "AKIAWYPXMNSELH6572H7"
+  default     = "AKIAWYPXMNSEAGN6M75Y"
 }
 
 variable "AWS_SECRET_KEY" {
   type        = string
   description = "AWS SECRET KEY"
-  default     = "Kc0TorUHdAW6tmYrSlGTwb2nX30/c4tI62IEYcrL"
+  default     = "2623rKvuVFeaBehMoAS/c826+oycvFFtpPwqOBFC"
 }
 
 variable "aws_region" {
